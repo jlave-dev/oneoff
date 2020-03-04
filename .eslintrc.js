3ddeeb64c4fb23157b8aa1b035b18696d1c0bcc3
James Lave <jlave.dev@gmail.com>
2020-03-03 23:07:40 -0600

    Changes to config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
+
   overrides: [
     {
       files: [
@@ -26,4 +31,10 @@ module.exports = {
       },
     },
   ],
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    '@vue/typescript/recommended'
+  ]
 };
